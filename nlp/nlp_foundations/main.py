8d6cf602d0930d690b3db0a960112f4c29fa371f
mwahajkhalil <mwahajkhalil@gmail.com>
2026-05-08 12:26:23 +0500

    first push

---
+++ nlp/nlp_foundations/main.py
@@ -0,0 +1,16 @@
+
+
+# writing a function for NLP tokenization
+def tokenize(text):
+    return text.split()
+
+
+input_text = input("Enter the Text to get Tokenize: ")
+print(tokenize(input_text))
+
+
+
+# Tokenization is the process of breaking down text into smaller units such as words, phrases, or symbols called tokens.
+
+
+
